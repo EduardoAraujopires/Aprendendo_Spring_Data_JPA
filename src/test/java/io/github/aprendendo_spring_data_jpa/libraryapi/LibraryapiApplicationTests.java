4fa6702f603e9b94beada1330b62344e768a39bf
Eduardo Araújo <eduaraujopires370@gmail.com>
2026-01-30 22:08:33 -0300

    Criando o docker e salvando os comandos

---
+++ src/test/java/io/github/aprendendo_spring_data_jpa/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aprendendo_spring_data_jpa.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
